ffd01cef0ce7fa1c7475c370fd6d148d2baa329c
eniallator <eniallatormc@gmail.com>
2017-01-02 22:32:49 +0000

    Added a refueling feature if the turtle ever runs out of fuel on a job

--- lib/doTurtle.lua
+++ lib/doTurtle.lua
@@ -14,13 +14,40 @@ local function selectNextItem()
   return found
 end
 
+local function refuel()
+  local refueled = false
+
+  for i=1,16 do
+    if turtle.getItemCount(i) > 0 then
+      turtle.select(i)
+
+      if turtle.refuel(turtle.getItemCount()) then
+        refueled = true
+      end
+    end
+  end
+
+  if not refueled then
+    print("Out of fuel! put some fuel in my inventory and i will use it.")
+    sleep(5)
+  end
+end
+
 doTurtle.placeUp = function()
-  while not selectNextItem() do sleep(5) print("Out of blocks!") end
+  while not selectNextItem() do
+    print("Out of blocks!")
+    sleep(5)
+  end
+
   while not turtle.placeUp() and not turtle.detectUp() do end
 end
 
 doTurtle.forward = function()
-  while not turtle.forward() do end
+  while not turtle.forward() do
+    if turtle.getFuelLevel() == 0 then
+      refuel()
+    end
+  end
 end
 
 return doTurtle
