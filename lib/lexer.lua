f8b89fc1662f6604a566541936f5d4068b03f61e
Bryan <dogescript@gmail.com>
2017-01-02 13:00:54 -0700

    Add lexer and parser for input templates
    Lexer takes in a string, returns a token list. Parser takes in a token list, returns template outline.

---
+++ lib/lexer.lua
@@ -0,0 +1,200 @@
+local buffer = {}
+
+local strByte = string.byte
+
+-- A consuming buffer, so storing index is irrelavent
+function buffer.new(str)
+  local t = {contents = str:sub(2), c = str:sub(1, 1), ln = 1, cr = 0}
+  setmetatable(t, {__index = buffer})
+
+  return t
+end
+
+function buffer:peek(d)
+  d = d or 0
+  return self.contents:sub(d + 1, d + 1)
+end
+
+function buffer:next()
+  local p = self.contents:sub(1, 1)
+  self.contents = self.contents:sub(2)
+  self.c = p
+
+  if p == "\n" then
+    self.ln = self.ln + 1
+    self.cr = 0
+  else
+    self.cr = self.cr + 1
+  end
+
+  return p
+end
+
+function buffer:hasNext()
+  return #self.contents > 0
+end
+
+local lexer = {}
+
+local function tokenizeInstruction(buffer)
+  local co = buffer.c:match("[a-zA-Z_]+")
+  if co then
+    local str = ""
+    while buffer.c:match("[a-zA-Z0-9_]+") do
+      str = str .. buffer.c
+      buffer:next()
+    end
+
+    return {type="instruction", content=str, lp = buffer.ln}
+  end
+  return false
+end
+
+local escapeLookup = {r="\r", n="\n", b="\b", ["\""]="\"", ["\\"]="\\"}
+local function escapeChar(c)
+  local l = escapeLookup[c]
+  return l and l or c
+end
+
+local function tokenizeString(buffer)
+  if buffer.c == "\"" then
+    local lpos = buffer.ln
+    local cpos = buffer.cr
+
+    local its = ""
+    local brokeClean = false
+    while buffer:hasNext() do
+      local c = buffer:next()
+      if c == "\"" then
+        brokeClean = true
+        break
+      end
+      if c == "\\" then
+        c = escapeChar(buffer:next())
+      end
+      its = its .. c
+    end
+
+    if not brokeClean then
+      lexer.exception("Unterminated string at "..lpos..":"..cpos)
+    end
+
+    buffer:next()
+    return {type="string", content=its, lp = buffer.ln}
+  else
+    return false
+  end
+end
+
+local symbolLookup = {
+  ["{"] = "open-bracket",
+  ["}"] = "close-bracket",
+  ["("] = "open-arg",
+  [")"] = "close-arg",
+  [","] = "arg-sep",
+  ["->"] = "proceed"
+}
+local function tokenizeSymbol(buffer)
+  local val = symbolLookup[buffer.c]
+  if val then
+    buffer:next()
+    return {type="symbol", content=val, lp = buffer.ln}
+  else
+    local nxt = buffer:peek()
+    val = symbolLookup[buffer.c .. nxt]
+    if val then
+      buffer:next()
+      buffer:next()
+      return {type="symbol", content=val, lp = buffer.ln}
+    else
+      return false
+    end
+  end
+end
+
+local startNum = 48
+local function isNum(c)
+  local b = strByte(c)
+  if b >= startNum and b < startNum + 10 then
+    --WOO ITS A NUMBER (or at least part of one)
+    return true
+  end
+end
+
+local function tokenizeNumber(buffer)
+  if isNum(buffer.c) then
+    local strN = buffer.c
+    while buffer:hasNext() and isNum(buffer:next()) do
+      strN = strN .. buffer.c
+    end
+    if not tonumber(strN) then
+      lexer.exception("NaN: "..strN)
+    end
+    return {type="number", content=tonumber(strN), lp = buffer.ln}
+  end
+  return false
+end
+
+local function tokenizeComment(buffer)
+  if buffer.c == "-" and buffer:peek() == "-" then
+    local str = ""
+    while buffer:hasNext() and buffer:peek() ~= "\n" do
+      str = str .. buffer:next()
+    end
+    buffer:next()
+    return {type="comment", content=str, lp = buffer.ln}
+  end
+  return false
+end
+
+local function consumeWhitespace(buffer)
+  while buffer:hasNext() do
+    if buffer.c:find("%s") then
+      buffer:next()
+    else
+      break
+    end
+  end
+end
+
+function lexer.lex(code)
+  if #code == 0 then return {} end -- sanity check
+
+  code = code .. " " -- cuz stuff was being munched
+
+  local tList = {}
+  local buffer = buffer.new(code)
+  local t
+
+  repeat
+    local bad = 0
+    consumeWhitespace(buffer)
+
+    t = tokenizeComment(buffer)
+    if t then table.insert(tList, t) else bad = bad + 1 end
+
+    t = tokenizeString(buffer)
+    if t then table.insert(tList, t) else bad = bad + 1 end
+
+    t = tokenizeSymbol(buffer)
+    if t then table.insert(tList, t) else bad = bad + 1 end
+
+    t = tokenizeNumber(buffer)
+    if t then table.insert(tList, t) else bad = bad + 1 end
+
+    t = tokenizeInstruction(buffer)
+    if t then table.insert(tList, t) else bad = bad + 1 end
+
+    if bad == 5 then
+      lexer.exception("Unexpected character at "..buffer.ln..":"..buffer.cr)
+    end
+  until not buffer:hasNext()
+
+  return tList
+end
+
+function lexer.exception(msg)
+  error(msg, 4)
+end
+
+return lexer
---
+++ lib/parser.lua
@@ -0,0 +1,143 @@
+local parser = {}
+
+local tabInsert = table.insert
+local tabRemove = table.remove
+local tabConcat = table.concat
+
+local function consumeToken(tList, tType, tCont, hard)
+  if #tList > 0 then -- Sanity check
+    local nToken = tabRemove(tList, 1)
+
+    local lkTb = {}
+    if type(tType) == "string" then
+      tType = {tType}
+    end
+
+    for i=1, #tType do
+      lkTb[tType[i]] = true
+    end
+
+    if not lkTb[nToken.type] then
+      if hard then
+        error("Expected "..tabConcat(tType, " or ")..", got "..nToken.type.." on line "..nToken.lp)
+      else
+        return false, nToken
+      end
+    end
+
+    if tCont then
+      if nToken.content ~= tCont then
+        if hard then
+          error("Expected '"..tabConcat(tType, " or ").."' "..tType..", got '"..nToken.content.."' on line "..nToken.lp)
+        else
+          return false, nToken
+        end
+      end
+    end
+
+    return true, nToken
+  elseif hard then
+    error("Expected "..tType..", got EOF")
+  end
+end
+
+local turtleInstructions = {
+  forward = true,
+  rotate = true
+}
+local function consumeTurtleInstruction(nxt, tList, form)
+  local s, instr
+  if nxt then
+    s, instr = true, nxt
+  else
+    s, instr = consumeToken(tList, {"instruction", "symbol"}, nil, true)
+  end
+  if turtleInstructions[instr.content] then
+    local oarg = consumeToken(tList, "symbol", "open-arg", true)
+    local argoc
+    local alist = {}
+    repeat
+      s, argoc = consumeToken(tList, {"number", "symbol"}, nil, true)
+      if argoc.type == "symbol" then
+        if argoc.content == "close-arg" then
+          return true, instr.content, alist
+        elseif argoc.content ~= "arg-sep" then
+          error("Unexpected symbol '"..argoc.content.."' on line "..instr.lp)
+        end
+      else
+        tabInsert(alist, argoc.content)
+      end
+    until argoc.type == "symbol" and argoc.content == "close-arg"
+  elseif (instr.type == "symbol" and instr.content == "close-bracket") then
+    return false
+  elseif instr.type == "symbol" and instr.content ~= "close-bracket" then
+    error("Unexpected symbol '"..instr.content.."' on line "..instr.lp)
+  else
+    error("Unknown turtle instruction '"..instr.content.."' on line "..instr.lp)
+  end
+end
+
+local instructions = {
+  start_string = function(tList, form)
+    local succ, ss = consumeToken(tList, "string", nil, true)
+    form.ss = ss.content
+  end,
+  rule = function(tList, form)
+    local s, name = consumeToken(tList, "string", nil, true)
+    local s, sChek = consumeToken(tList, "symbol", "proceed", true)
+    local s, endr = consumeToken(tList, "string", nil, true)
+
+    tabInsert(form.rules, {name.content, endr.content})
+  end,
+  ["function"] = function(tList, form)
+    local s, name = consumeToken(tList, "string", nil, true)
+    local s, nxt = consumeToken(tList, {"instruction", "symbol"}, nil, true)
+
+    local parts = {}
+
+    if nxt.type == "symbol" then
+      if nxt.content == "open-bracket" then
+        while true do
+          local s, n, a = consumeTurtleInstruction(nil, tList, form)
+          if not s then break end
+
+          tabInsert(parts, {n, a})
+        end
+      else
+        error("Expected 'open-bracket' symbol, got "..nxt.content.." on line "..nxt.lp)
+      end
+    else
+      local s, n, a = consumeTurtleInstruction(nxt, tList, form)
+      if s then
+        parts = {{n, a}}
+      end
+    end
+
+    tabInsert(form.funcs, {name.content, parts})
+  end
+}
+
+function parser.parse(tokenList)
+  local form = {ss="", rules={}, funcs={}}
+
+  while #tokenList > 0 do
+    local succ, currToken = consumeToken(tokenList, "instruction", nil, false)
+
+    if not succ then
+      if currToken.type ~= "comment" then
+        error("Expected instruction, got "..currToken.type.." on line "..currToken.lp)
+      end
+    else
+      local ins = instructions[currToken.content]
+      if not ins then
+        error("Unknown instruction '"..currToken.content.."' on line "..currToken.lp)
+      end
+
+      ins(tokenList, form)
+    end
+  end
+
+  return form
+end
+
+return parser
